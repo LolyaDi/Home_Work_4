d72da07f3a1269c8da8ea247e5b5ab5e5d488630
LolyaDi <you@example.com>
2019-03-23 01:13:03 +0600

    Четвертая домашняя работа

---
+++ HomeWork_4/Motorcycle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace HomeWork_4
+{
+    public partial class Motorcycle
+    {
+        public string Brand { get; set; }
+        
+        public string Model { get; set; }
+
+        public string Type { get; set; }
+
+        public string PhotoAddress { get; set; }
+
+        public int Horsepower { get; set; }
+
+        public double Torque { get; set; }
+
+        public int MaxSpeed { get; set; }
+
+        public static int AverageSpeed { get; set; }
+
+        public Motorcycle()
+        {
+            const int YZF_R3_MAX_SPEED = 175;
+            const int YZF_R3_HORSEPOWER = 42;
+            const double YZF_R3_TORQUE = 29.6;
+
+            Brand = "Yamaha";
+            Model = "YZF-R3";
+            Type = "Superbike";
+            MaxSpeed = YZF_R3_MAX_SPEED;
+            Horsepower = YZF_R3_HORSEPOWER;
+            Torque = YZF_R3_TORQUE;
+            PhotoAddress = "https://www.kupimoto.com.ua/wp-content/uploads/2016/02/2016-Yamaha-YZF-R320-EU-Matt-Grey-Studio-002_gal_full.jpg";
+        }
+
+        static Motorcycle()
+        {
+            const int AVERAGE_SPEED_OF_MOTORCYCLE = 90;
+            AverageSpeed = AVERAGE_SPEED_OF_MOTORCYCLE;
+        }
+
+        public void MotorcycleInitialization()
+        {
+            WriteLine("Введите марку мотоцикла - ");
+            Brand = ReadLine();
+            WriteLine("Введите модель мотоцикла -");
+            Model = ReadLine();
+            WriteLine("Введите тип(Спортивный) мотоцикла - ");
+            Type = ReadLine();
+            WriteLine("Введите максимальную скорость мотоцикла (км/ч) - ");
+            MaxSpeed = int.Parse(ReadLine());
+            WriteLine("Введите количество лошадинных сил мотоцикла - ");
+            Horsepower = int.Parse(ReadLine());
+            WriteLine($"Введите URL адрес фото мотоцикла {Brand} - {Model} -");
+            PhotoAddress = ReadLine();
+        }
+
+        public void OutputCharacteristicsMotorcycle()
+        {
+            WriteLine();
+            WriteLine();
+            WriteLine($"Основные характеристики мотоцикла - {Brand} - {Model}");
+            WriteLine($"Тип - {Type}");
+            WriteLine($"Максимальная скорость - {MaxSpeed} км/ч");
+            WriteLine($"Средняя скорость - {AverageSpeed} км/ч");
+            WriteLine($"Мощность - {Horsepower} л.с");
+            WriteLine($"URL адрес - {PhotoAddress}");
+        }
+    }
+}
---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace HomeWork_4
+{
+    public partial class Motorcycle
+    {
+        public void CalculateAcceleration(ref Motorcycle motorcycle)
+        {
+            const int ONE_KILOMETR_IN_METERS = 1000;
+            const int ONE_HOUR_IN_SECONDS = 3600;
+            double SpeedInMetersPerSecond = motorcycle.MaxSpeed * ONE_KILOMETR_IN_METERS / ONE_HOUR_IN_SECONDS;
+
+            WriteLine($"Ускорение мотоцикла {motorcycle.Brand} - {motorcycle.Model} за 10 секунд ,с максимальной скоростью {SpeedInMetersPerSecond} м/с =  {SpeedInMetersPerSecond / 10.0} м/с^2");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int FIVE = 5;
+            Motorcycle motorcycle = new Motorcycle();
+            Motorcycle[] motorcycles = new Motorcycle[FIVE];
+
+            motorcycle.MotorcycleInitialization();
+            motorcycle.OutputCharacteristicsMotorcycle();
+            motorcycle.CalculateAcceleration(ref motorcycle);
+
+
+            WriteLine("Вывод конструктора по умолчанию - ");
+            for(int i = 0; i < motorcycles.Length; i++)
+            {
+                motorcycles[i] = new Motorcycle();
+                motorcycles[i].OutputCharacteristicsMotorcycle();
+                motorcycles[i].CalculateAcceleration(ref motorcycles[i]);
+                break;
+            }
+
+            ReadKey();
+        }
+    }
+}
